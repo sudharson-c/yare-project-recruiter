9fa50cf8a7ad72b55f6d8666ded022f20e8fe2bf
sudharson-c <sudharson@student.tce.edu>
2024-12-02 12:31:00 +0530

    Update EditModal.jsx

--- client/src/components/sub/EditModal.jsx
+++ client/src/components/sub/EditModal.jsx
@@ -75,7 +75,7 @@ const EditModal = ({ project, close }) => {
               </div>
               <div className="col-span-2">
                 <label
-                  htmlFor="project_desc"
+                  htmlFor="description"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Project Description:
@@ -146,16 +146,16 @@ const EditModal = ({ project, close }) => {
               </div>
               <div className="col-span-2">
                 <label
-                  htmlFor="status"
+                  htmlFor="project_status"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Enter the status
                 </label>
                 <select
-                  id="status"
-                  name="status"
+                  id="project_status"
+                  name="project_status"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  value={projectDetails.status}
+                  value={projectDetails.project_status}
                   onChange={handleChange}
                 >
                   <option value={"NEW"}>NEW</option>
